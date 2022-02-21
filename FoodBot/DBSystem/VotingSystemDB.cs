96fd0b84aa81ce0c4fb4e0c1bea49d22eea2fba4
Artem Yurchenko <zumikq78@gmail.com>
2022-02-21 19:11:02 +0100

    VotingSystemDB - code was formatted

--- FoodBot/DBSystem/VotingSystemDB.cs
+++ FoodBot/DBSystem/VotingSystemDB.cs
@@ -34,87 +34,87 @@ public class VotingSystemDB : DbContext
 
 		return null;
 	}
-	
+
 	public bool TryGetVotingStartParametersByChatIdentifier (DiscordChatIdentifier chatID, out VotingStartParameters? foundParameters)
 	{
 		foundParameters = VotingStartParameters.SingleOrDefault(votingStartParameters => votingStartParameters.ChatIdentifier == chatID);
 
 		return foundParameters != null;
 	}
-	
+
 	public string? RemoveVotingStartParametersByChatIdentifier (DiscordChatIdentifier chatID)
 	{
 		if (TryGetVotingStartParametersByChatIdentifier(chatID, out VotingStartParameters? foundParameters) == false)
 		{
 			return "No voting start parameters for this chat!";
 		}
-		
+
 		Remove(foundParameters);
 		SaveChanges();
 
 		return null;
 	}
-	
+
 	public string? AddVotingParameters (VotingParameters newVotingParameters)
 	{
 		if (TryGetVotingParametersByChatIdentifier(newVotingParameters.ChatIdentifier, out _) == true)
 		{
 			return "Voting parameters already exists for this chat!";
 		}
-		
+
 		Add(newVotingParameters);
 		SaveChanges();
-		
+
 		return null;
 	}
-	
+
 	public bool TryGetVotingParametersByChatIdentifier (DiscordChatIdentifier chatID, out VotingParameters? foundParameters)
 	{
 		foundParameters = VotingParameters.SingleOrDefault(votingParameters => votingParameters.ChatIdentifier == chatID);
 
 		return foundParameters != null;
 	}
-	
+
 	public string? RemoveVotingParametersByChatIdentifier (DiscordChatIdentifier chatID)
 	{
 		if (TryGetVotingParametersByChatIdentifier(chatID, out VotingParameters? foundParameters) == false)
 		{
 			return "No voting parameters for this chat!";
 		}
-		
+
 		Remove(foundParameters);
 		SaveChanges();
 
 		return null;
 	}
-	
+
 	public string? AddVotingEndParameters (VotingEndParameters newVotingEndParameters)
 	{
 		if (TryGetVotingEndParametersByChatIdentifier(newVotingEndParameters.ChatIdentifier, out _) == true)
 		{
 			return "Voting end parameters already exists for this chat!";
 		}
-		
+
 		Add(newVotingEndParameters);
 		SaveChanges();
 
 		return null;
 	}
-	
+
 	public bool TryGetVotingEndParametersByChatIdentifier (DiscordChatIdentifier chatID, out VotingEndParameters? foundParameters)
 	{
 		foundParameters = VotingEndParameters.SingleOrDefault(votingParameters => votingParameters.ChatIdentifier == chatID);
 
 		return foundParameters != null;
 	}
-	
+
 	public string? RemoveVotingEndParametersByChatIdentifier (DiscordChatIdentifier chatID)
 	{
 		if (TryGetVotingEndParametersByChatIdentifier(chatID, out VotingEndParameters? foundParameters) == false)
 		{
 			return "No voting end parameters for this chat!";
 		}
-		
+
 		Remove(foundParameters);
 		SaveChanges();
 
