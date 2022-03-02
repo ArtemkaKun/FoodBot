54e820dcf5fce086641922c3cdc68cbe6b1d164e
Artem Yurchenko <zumikq78@gmail.com>
2022-03-02 21:37:20 +0100

    VotingManager class - StartVotingThreads logic was implemented

--- FoodBot/VotingSystem/VotingManager.cs
+++ FoodBot/VotingSystem/VotingManager.cs
@@ -1,13 +1,72 @@
-using FoodBot.DBSystem;
-
 namespace FoodBot.VotingSystem;
 
 public class VotingManager
 {
-	private readonly VotingSystemDB testVotingSystemDB = new();
-	
-	public void Initialize ()
+	private IReadOnlyList<VotingParameters> VotingParametersList { get; set; }
+	private CancellationTokenSource VotingThreadsCancellationToken { get; set; } = null!;
+
+	public VotingManager (IReadOnlyList<VotingParameters> votingParametersList)
 	{
-		testVotingSystemDB.Initialize();
+		VotingParametersList = votingParametersList;
+	}
+
+	public void StartVotingThreads ()
+	{
+		VotingThreadsCancellationToken = new CancellationTokenSource();
+
+		foreach (VotingParameters votingParameters in VotingParametersList)
+		{
+			DoVotingProcess(votingParameters, VotingThreadsCancellationToken.Token);
+		}
+	}
+
+	private async Task DoVotingProcess (VotingParameters parameters, CancellationToken cancellationToken)
+	{
+		TimeSpan currentTime = DateTime.Now.TimeOfDay;
+		TimeSpan timeToWait = currentTime < parameters.startTime ? parameters.startTime.Subtract(currentTime) : DateTime.Today.Subtract(currentTime).TimeOfDay + parameters.startTime;
+
+		try
+		{
+			await Task.Delay(timeToWait, cancellationToken);
+		}
+		catch (TaskCanceledException _)
+		{
+			return;
+		}
+
+		while (true)
+		{
+			if (CheckIfNotifyToday() == true)
+			{
+				//notify start
+				
+				try
+				{
+					await Task.Delay(TimeSpan.FromMinutes(parameters.durationInMinutes), cancellationToken);
+				}
+				catch (TaskCanceledException _)
+				{
+					return;
+				}
+				
+				//notify end
+
+				try
+				{
+					await Task.Delay(new TimeSpan(24, 0, 0), cancellationToken);
+				}
+				catch (TaskCanceledException _)
+				{
+					return;
+				}
+			}
+		}
+	}
+
+	private bool CheckIfNotifyToday ()
+	{
+		DayOfWeek todayDay = DateTime.Today.DayOfWeek;
+
+		return todayDay != DayOfWeek.Saturday && todayDay != DayOfWeek.Sunday;
 	}
 }
