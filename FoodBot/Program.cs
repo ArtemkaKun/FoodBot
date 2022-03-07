89c37b65a73d9e2bdf8f9b6143c1ea0904e85dd3
Artem Yurchenko <zumikq78@gmail.com>
2022-03-07 20:48:50 +0100

    Program - VotingManager object creation was moved to Main method

--- FoodBot/Program.cs
+++ FoodBot/Program.cs
@@ -6,11 +6,12 @@ namespace FoodBot;
 public static class Program
 {
 	private static VotingSystemDB VotingSystemDB { get; } = new();
-	private static VotingManager VotingManager { get; } = new(VotingSystemDB);
+	private static VotingManager VotingManager { get; set; } = null!;
 
 	public static void Main ()
 	{
 		VotingSystemDB.Initialize();
+		VotingManager = new VotingManager(VotingSystemDB);
 		VotingManager.StartVotingThreads();
 	}
 }
