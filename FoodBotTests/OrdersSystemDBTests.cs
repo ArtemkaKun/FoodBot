0eae4a37ab23430a49770924266099ad314807c2
Artem Yurchenko <zumikq78@gmail.com>
2022-04-22 21:49:40 +0200

    OrderSystemDBTests class - local db was changed on the Program one; OneTime fixtures were changed on every test siblings; class and all tests were marked as NonParallelizable

--- FoodBotTests/OrdersSystemDBTests.cs
+++ FoodBotTests/OrdersSystemDBTests.cs
@@ -2,15 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using FoodBot;
 using FoodBot.OrdersSystem;
 using NUnit.Framework;
 
 namespace FoodBotTests;
 
+[NonParallelizable]
 public class OrdersSystemDBTests
 {
-	private readonly OrdersSystemDB ordersSystemDB = new();
-	
 	// TODO same code as in VotingSystemDBTests. 30.03.2022. Artem Yurchenko
 	private const ulong TEST_GUILD_ID = 0;
 	private const ulong TEST_CHANNEL_ID = 0;
@@ -24,23 +24,23 @@ public class OrdersSystemDBTests
 		Text = "Test"
 	};
 	
-	[OneTimeSetUp]
+	[SetUp]
 	public void InitializeDB()
 	{
-		ordersSystemDB.Initialize();
+		Program.OrdersSystemDB.Initialize();
 	}
-
-	[OneTimeTearDown]
-	public void Terminate ()
+	
+	[TearDown]
+	public void CleanDB()
 	{
-		File.Delete("./OrdersData.db");
+		Program.OrdersSystemDB.Terminate();
 	}
 
-	[Test]
+	[Test, NonParallelizable]
 	public void AddTestOrder_PresentAndValid_True ()
 	{
-		ordersSystemDB.AddOrder(testOrder);
-		List<Order> foundOrders = ordersSystemDB.GetTodayOrdersByChatIdentifier(testOrder.GuildID, testOrder.ChannelID);
+		Program.OrdersSystemDB.AddOrder(testOrder);
+		List<Order> foundOrders = Program.OrdersSystemDB.GetTodayOrdersByChatIdentifier(testOrder.GuildID, testOrder.ChannelID);
 		
 		if (foundOrders.Count == 0 || foundOrders.Contains(testOrder) == false)
 		{
@@ -48,17 +48,17 @@ public class OrdersSystemDBTests
 			return;
 		}
 
-		ordersSystemDB.RemoveOrder(testOrder);
+		Program.OrdersSystemDB.RemoveOrder(testOrder);
 		Assert.Pass();
 	}
 
-	[Test]
+	[Test, NonParallelizable]
 	public void AddTestOrderAndRemove_PresentInDB_False ()
 	{
-		ordersSystemDB.AddOrder(testOrder);
-		ordersSystemDB.RemoveOrder(testOrder);
+		Program.OrdersSystemDB.AddOrder(testOrder);
+		Program.OrdersSystemDB.RemoveOrder(testOrder);
 		
-		List<Order> foundOrders = ordersSystemDB.GetTodayOrdersByChatIdentifier(testOrder.GuildID, testOrder.ChannelID);
+		List<Order> foundOrders = Program.OrdersSystemDB.GetTodayOrdersByChatIdentifier(testOrder.GuildID, testOrder.ChannelID);
 		
 		if (foundOrders.Count != 0)
 		{
@@ -69,7 +69,7 @@ public class OrdersSystemDBTests
 		Assert.Pass();
 	}
 
-	[Test]
+	[Test, NonParallelizable]
 	public void AddTwoOrders_BothPresentAndValid_True ()
 	{
 		Order secondTestOrder = new()
@@ -81,10 +81,10 @@ public class OrdersSystemDBTests
 			Text = "Test2"
 		};
 		
-		ordersSystemDB.AddOrder(testOrder);
-		ordersSystemDB.AddOrder(secondTestOrder);
+		Program.OrdersSystemDB.AddOrder(testOrder);
+		Program.OrdersSystemDB.AddOrder(secondTestOrder);
 		
-		List<Order> foundOrders = ordersSystemDB.GetTodayOrdersByChatIdentifier(testOrder.GuildID, testOrder.ChannelID);
+		List<Order> foundOrders = Program.OrdersSystemDB.GetTodayOrdersByChatIdentifier(testOrder.GuildID, testOrder.ChannelID);
 		
 		if (foundOrders.Count == 0 || foundOrders.Contains(testOrder) == false || foundOrders.Contains(secondTestOrder) == false)
 		{
@@ -92,23 +92,23 @@ public class OrdersSystemDBTests
 			return;
 		}
 		
-		ordersSystemDB.RemoveOrder(testOrder);
-		ordersSystemDB.RemoveOrder(secondTestOrder);
+		Program.OrdersSystemDB.RemoveOrder(testOrder);
+		Program.OrdersSystemDB.RemoveOrder(secondTestOrder);
 		Assert.Pass();
 	}
 	
-	[Test]
+	[Test, NonParallelizable]
 	public void AddTestOrderAndUpdateIt_PresentAndValid_True ()
 	{
-		ordersSystemDB.AddOrder(testOrder);
+		Program.OrdersSystemDB.AddOrder(testOrder);
 
-		if (ordersSystemDB.TryUpdateOrderTextByChatIdentifierAndID(testOrder.GuildID, testOrder.ChannelID, testOrder.ID, "Test2") == false)
+		if (Program.OrdersSystemDB.TryUpdateOrderTextByChatIdentifierAndID(testOrder.GuildID, testOrder.ChannelID, testOrder.ID, "Test2") == false)
 		{
 			Assert.Fail("Order update was failed");
 			return;
 		}
 		
-		List<Order> foundOrders = ordersSystemDB.GetTodayOrdersByChatIdentifier(testOrder.GuildID, testOrder.ChannelID);
+		List<Order> foundOrders = Program.OrdersSystemDB.GetTodayOrdersByChatIdentifier(testOrder.GuildID, testOrder.ChannelID);
 		
 		if (foundOrders.Count == 0 || foundOrders.Find(order => order.Text == "Test2") == null)
 		{
@@ -116,22 +116,22 @@ public class OrdersSystemDBTests
 			return;
 		}
 
-		ordersSystemDB.RemoveOrder(testOrder);
+		Program.OrdersSystemDB.RemoveOrder(testOrder);
 		Assert.Pass();
 	}
 
-	[Test]
+	[Test, NonParallelizable]
 	public void AddTestOrderAndRemoveItById_NotPresentInDB_True ()
 	{
-		ordersSystemDB.AddOrder(testOrder);
+		Program.OrdersSystemDB.AddOrder(testOrder);
 
-		if (ordersSystemDB.TryRemoveOrderByChatIdentifierAndID(testOrder.GuildID, testOrder.ChannelID, testOrder.ID) == false)
+		if (Program.OrdersSystemDB.TryRemoveOrderByChatIdentifierAndID(testOrder.GuildID, testOrder.ChannelID, testOrder.ID) == false)
 		{
 			Assert.Fail("Order removing was failed");
 			return;
 		}
 		
-		List<Order> foundOrders = ordersSystemDB.GetTodayOrdersByChatIdentifier(testOrder.GuildID, testOrder.ChannelID);
+		List<Order> foundOrders = Program.OrdersSystemDB.GetTodayOrdersByChatIdentifier(testOrder.GuildID, testOrder.ChannelID);
 		
 		if (foundOrders.Count != 0)
 		{
@@ -142,7 +142,7 @@ public class OrdersSystemDBTests
 		Assert.Pass();
 	}
 
-	[Test]
+	[Test, NonParallelizable]
 	public void AddTwoTestOrdersAndGetOnlyForOneUser_ReturnedOnlyOneValidOrder_True ()
 	{
 		Order secondTestOrder = new()
@@ -154,10 +154,10 @@ public class OrdersSystemDBTests
 			Text = "Test"
 		};
 		
-		ordersSystemDB.AddOrder(testOrder);
-		ordersSystemDB.AddOrder(secondTestOrder);
+		Program.OrdersSystemDB.AddOrder(testOrder);
+		Program.OrdersSystemDB.AddOrder(secondTestOrder);
 		
-		List<Order> foundOrders = ordersSystemDB.GetTodayOrdersByChatIdentifierAndPersonName(testOrder.GuildID, testOrder.ChannelID, testOrder.PersonName);
+		List<Order> foundOrders = Program.OrdersSystemDB.GetTodayOrdersByChatIdentifierAndPersonName(testOrder.GuildID, testOrder.ChannelID, testOrder.PersonName);
 		
 		if (foundOrders.Count is 0 or not 1 || foundOrders.Contains(testOrder) == false)
 		{
@@ -165,8 +165,8 @@ public class OrdersSystemDBTests
 			return;
 		}
 		
-		ordersSystemDB.RemoveOrder(testOrder);
-		ordersSystemDB.RemoveOrder(secondTestOrder);
+		Program.OrdersSystemDB.RemoveOrder(testOrder);
+		Program.OrdersSystemDB.RemoveOrder(secondTestOrder);
 		Assert.Pass();
 	}
 }
