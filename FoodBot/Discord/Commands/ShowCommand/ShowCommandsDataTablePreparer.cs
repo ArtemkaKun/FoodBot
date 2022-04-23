09d9c4ce698de0c63f7a5d599c7d79f38f17e674
Artem Yurchenko <zumikq78@gmail.com>
2022-04-23 12:03:17 +0200

    ShowCommandsDataTablePreparerTests  - ConvertCountedOrdersToDataTable_ReturnedDataTableEqualToExpected_True test was implemented and passed

--- FoodBot/Discord/Commands/ShowCommand/ShowCommandsDataTablePreparer.cs
+++ FoodBot/Discord/Commands/ShowCommand/ShowCommandsDataTablePreparer.cs
@@ -9,6 +9,7 @@ public static class ShowCommandsDataTablePreparer
 	private const string ID_COLUMN_NAME = "Commands";
 	private const string PERSON_COLUMN_NAME = "Description";
 	private const string ORDER_COLUMN_NAME = "Order";
+	private const string COUNT_COLUMN_NAME = "Count";
 	
 	public static DataTable GetOrdersDataTable (List<Order> orders)
 	{
@@ -22,6 +23,22 @@ public static class ShowCommandsDataTablePreparer
 			ordersTable.Rows.Add(order.ID, order.PersonName, order.Text);
 		}
 		
+		return ordersTable;
+	}
+
+	public static DataTable GetCountedOrdersDataTable (List<Order> orders)
+	{
+		DataTable ordersTable = new();
+		ordersTable.Columns.Add(ORDER_COLUMN_NAME, typeof(string));
+		ordersTable.Columns.Add(COUNT_COLUMN_NAME, typeof(uint));
+
+		Dictionary<string, int> countedOrders = OrdersListPreparer.CountOrders(orders);
+
+		foreach (KeyValuePair<string, int> countedOrder in countedOrders)
+		{
+			ordersTable.Rows.Add(countedOrder.Key, countedOrder.Value);
+		}
+
 		return ordersTable;
 	}
 }
--- FoodBotTests/ShowCommandsDataTablePreparerTests.cs
+++ FoodBotTests/ShowCommandsDataTablePreparerTests.cs
@@ -121,4 +121,78 @@ public class ShowCommandsDataTablePreparerTests
 			}
 		}
 	}
+	
+	[Test]
+	public void ConvertCountedOrdersToDataTable_ReturnedDataTableEqualToExpected_True ()
+	{
+		DataTable expectedDataTable = new();
+		// TODO same values as in the ShowCommandsDataTablePreparer class. 23.04.2022. Artem Yurchenko
+		expectedDataTable.Columns.Add("Order", typeof(string));
+		expectedDataTable.Columns.Add("Count", typeof(uint));
+		expectedDataTable.Rows.Add("Test Text", 3);
+		expectedDataTable.Rows.Add("Test Text1", 2);
+		expectedDataTable.Rows.Add("Test Text2", 1);
+
+		List<Order> orders = new()
+		{
+			new Order
+			{
+				ID = 1,
+				PersonName = "Tets",
+				Text = "Test Text"
+			},
+			new Order
+			{
+				ID = 2,
+				PersonName = "Tets1",
+				Text = "Test Text"
+			},
+			new Order
+			{
+				ID = 3,
+				PersonName = "Tets2",
+				Text = "Test Text"
+			},
+			new Order
+			{
+				ID = 4,
+				PersonName = "Tets3",
+				Text = "Test Text1"
+			},
+			new Order
+			{
+				ID = 5,
+				PersonName = "Tets4",
+				Text = "Test Text2"
+			},
+			new Order
+			{
+				ID = 6,
+				PersonName = "Tets5",
+				Text = "Test Text1"
+			}
+		};
+
+		DataTable commandsDataTable = ShowCommandsDataTablePreparer.GetCountedOrdersDataTable(orders);
+
+		if (expectedDataTable.Columns.Count != commandsDataTable.Columns.Count)
+		{
+			Assert.Fail("The number of columns in the expected data table is not equal to the number of columns in the actual data table.");
+			return;
+		}
+
+		if (expectedDataTable.Rows.Count != commandsDataTable.Rows.Count)
+		{
+			Assert.Fail("The number of rows in the expected data table is not equal to the number of rows in the actual data table.");
+			return;
+		}
+
+		for (int columnIndex = 0; columnIndex < expectedDataTable.Columns.Count; columnIndex++)
+		{
+			for (int rowIndex = 0; rowIndex < expectedDataTable.Rows.Count; rowIndex++)
+			{
+				Assert.AreEqual(expectedDataTable.Rows[rowIndex][columnIndex], commandsDataTable.Rows[rowIndex][columnIndex]);
+			}
+		}
+	}
 }
