c360c973403eafb6e57ac6e75b75098b8d5fb1a4
Artem Yurchenko <zumikq78@gmail.com>
2022-04-23 10:50:36 +0200

    OrderCommands - real logic was added to command methods

--- FoodBot/Discord/Commands/OrderCommand/OrderCommands.cs
+++ FoodBot/Discord/Commands/OrderCommand/OrderCommands.cs
@@ -10,6 +10,7 @@ public class OrderCommands : ModuleBase<SocketCommandContext>
 	private const string UPDATE_USER_ORDER_COMMAND_DESCRIPTION = "Updates order with provided text";
 	private const string DELETE_ORDER_COMMAND_DESCRIPTION = "Deletes order";
 	private const string ORDER_WAS_SAVED_MESSAGE = ORDER_WAS_SUCCESSFULLY_TEMPLATE_MESSAGE + "proceeded";
+	private const string ORDER_ADD_FAILED = "Failed to add order";
 	private const string ORDER_WAS_UPDATED_MESSAGE = ORDER_WAS_SUCCESSFULLY_TEMPLATE_MESSAGE + "updated";
 	private const string ORDER_WAS_REMOVED = ORDER_WAS_SUCCESSFULLY_TEMPLATE_MESSAGE + "removed";
 	private const string ORDER_UPDATE_FAILED = "Failed to update order";
@@ -23,20 +24,35 @@ public class OrderCommands : ModuleBase<SocketCommandContext>
 	[Summary(CREATE_NEW_ORDER_COMMAND_DESCRIPTION)]
 	public Task CreateNewOrder ([Remainder] string orderText)
 	{
-		return null;
+		if (OrderCommandsResults.AddOrder(Context.Guild.Id, Context.Channel.Id, Context.User.Username, orderText) == true)
+		{
+			return ReplyAsync(ORDER_WAS_SAVED_MESSAGE);
+		}
+
+		return ReplyAsync(ORDER_ADD_FAILED);
 	}
 
 	[Command(UPDATE_COMMAND_NAME)]
 	[Summary(UPDATE_USER_ORDER_COMMAND_DESCRIPTION)]
-	public Task UpdateUserOrder (int orderID, [Remainder] string newOrderText)
+	public Task UpdateUserOrder (uint orderID, [Remainder] string newOrderText)
 	{
-		return null;
+		if (OrderCommandsResults.UpdateOrder(Context.Guild.Id, Context.Channel.Id, orderID, newOrderText) == true)
+		{
+			return ReplyAsync(ORDER_WAS_UPDATED_MESSAGE);
+		}
+
+		return ReplyAsync(ORDER_UPDATE_FAILED);
 	}
 
 	[Command(DELETE_COMMAND_NAME)]
 	[Summary(DELETE_ORDER_COMMAND_DESCRIPTION)]
-	public Task DeleteOrder (int orderID)
+	public Task DeleteOrder (uint orderID)
 	{
-		return null;
+		if (OrderCommandsResults.RemoveOrder(Context.Guild.Id, Context.Channel.Id, orderID) == true)
+		{
+			return ReplyAsync(ORDER_WAS_REMOVED);
+		}
+
+		return ReplyAsync(ORDER_DELETE_FAILED);
 	}
 }
