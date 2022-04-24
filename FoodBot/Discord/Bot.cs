abd971e980915b9c1716136be565d3765c923a61
Artem Yurchenko <zumikq78@gmail.com>
2022-04-24 20:08:17 +0200

    Bot class was implemented and set up

---
+++ FoodBot/Discord/Bot.cs
@@ -0,0 +1,83 @@
+using System.Reflection;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace FoodBot.Discord;
+
+public class Bot
+{
+	private const string ENVIRONMENT_VARIABLE_WITH_BOT_TOKEN = "FOOD_BOT_TOKEN";
+	private const char BOT_COMMAND_PREFIX = '!';
+
+	private DiscordSocketClient Client { get; set; } = new();
+	private CommandService Commands { get; set; } = new();
+
+	public async Task Initialize ()
+	{
+		SubscribeOnClientEvents();
+		await GetCommandsModule();
+	}
+
+	public async Task StartBot ()
+	{
+		await Client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable(ENVIRONMENT_VARIABLE_WITH_BOT_TOKEN));
+		await Client.StartAsync();
+	}
+
+	public async Task SendMessage (ulong guildID, ulong channelID, string messageText)
+	{
+		await Client.GetGuild(guildID).GetTextChannel(channelID).SendMessageAsync(messageText);
+	}
+
+	private void SubscribeOnClientEvents ()
+	{
+		Client.Log += ProceedLogMessage;
+		Client.MessageReceived += ProceedReceivedMessage;
+	}
+
+	private Task ProceedLogMessage (LogMessage msg)
+	{
+		Console.WriteLine(msg.ToString());
+
+		return Task.CompletedTask;
+	}
+
+	private async Task ProceedReceivedMessage (SocketMessage message)
+	{
+		if (message is not SocketUserMessage userMessage)
+		{
+			return;
+		}
+
+		if (CheckIfMessageFromBot(userMessage) == true)
+		{
+			return;
+		}
+
+		if (CheckIfTheMessageContainsCommand(userMessage, out int commandPrefixPosition) == false)
+		{
+			return;
+		}
+
+		SocketCommandContext context = new(Client, userMessage);
+		await Commands.ExecuteAsync(context, commandPrefixPosition, null);
+	}
+
+	private bool CheckIfMessageFromBot (SocketUserMessage message)
+	{
+		return message.Author.IsBot == true;
+	}
+
+	private bool CheckIfTheMessageContainsCommand (SocketUserMessage message, out int commandPrefixPosition)
+	{
+		commandPrefixPosition = 0;
+
+		return message.HasCharPrefix(BOT_COMMAND_PREFIX, ref commandPrefixPosition) == true;
+	}
+
+	private async Task GetCommandsModule ()
+	{
+		await Commands.AddModulesAsync(Assembly.GetEntryAssembly(), null);
+	}
+}
--- FoodBot/Program.cs
+++ FoodBot/Program.cs
@@ -1,21 +1,32 @@
-﻿using FoodBot.OrdersSystem;
+﻿using FoodBot.Discord;
+using FoodBot.OrdersSystem;
 using FoodBot.VotingSystem;
 
 namespace FoodBot;
 
 public static class Program
 {
+	public static Bot BotClient { get; } = new();
 	public static OrdersSystemDB OrdersSystemDB { get; } = new();
 	public static VotingSystemDB VotingSystemDB { get; } = new();
 	
 	private static VotingManager VotingManager { get; set; } = null!;
 
-	public static void Main ()
+	private static void Main ()
 	{
 		VotingSystemDB.Initialize();
 		VotingManager = new VotingManager(VotingSystemDB);
 		VotingManager.StartVotingThreads();
 		
 		OrdersSystemDB.Initialize();
+		
+		BotClient.Initialize().GetAwaiter().GetResult();
+		StartBot().GetAwaiter().GetResult();
+	}
+	
+	private static async Task StartBot ()
+	{
+		await BotClient.StartBot();
+		await Task.Delay(-1);
 	}
 }
